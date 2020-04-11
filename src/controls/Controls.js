ad70ecd5a302417ae7ed8a21c35096b4c061c4fd
chrisdennett <chrisdennett@gmail.com>
2020-04-11 18:41:36 +0100

    Made contols bg visible if canvas small

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -193,8 +193,7 @@ const ControlsStyle = styled.div`
   justify-content: space-around;
   color: white;
   width: ${(props) => (props.showControls ? "210px" : "50px")};
-
-  background: rgba(255, 255, 255, 0.2);
+  background: rgba(0, 0, 0, 0.8);
 
   fieldset {
     border-color: white;
